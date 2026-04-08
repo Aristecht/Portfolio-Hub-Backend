3f9d2bd22b15b014f785ca71f0a7648362b5381b
Aristarh <aristiktop8@gmail.com>
2026-04-08 22:07:00 +0500

    sasa

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        domain: config.get<string>('SESSION_DOMAIN') || undefined,
         maxAge: ms(config.getOrThrow<string | number>('SESSION_MAX_AGE')),
         httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
         secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
